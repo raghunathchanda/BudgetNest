48dfe5e6f7964f28002c75db8412219579af14a4
raghunathchanda <chandaraghunathreddy@gmail.com>
2026-04-27 00:03:31 +0530

    removed unnessary things in projecturls

--- BudgetNest/urls.py
+++ BudgetNest/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.shortcuts import redirect
+
 from django.urls import path,include
-from django.contrib.auth import views as auth_views
-def home_redirect(request):
-    return redirect('login')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
