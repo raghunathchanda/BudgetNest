1d598f31cdba54201a41714ac39b32cb7cb09ee8
raghunathchanda <chandaraghunathreddy@gmail.com>
2026-04-28 07:10:24 +0530

    added navbar connectiond and tested hyper link

--- BudgetNest/urls.py
+++ BudgetNest/urls.py
@@ -22,4 +22,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('', include('family.urls')),
 ]
--- family/views.py
+++ family/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def add_family(request):
+    return HttpResponse("add family page")
